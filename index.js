254cf0fe3aa3452a86ecd2b53b96b0c9f611e15a
Samuele S <samisama2002@gmail.com>
2018-08-18 14:35:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+// function add
 function add(a, b) {
   return a + b;
 }
+// add's argument
 add(5, 5);
+// function subtract 
+function subtract(a, b) {
+  return a - b;
+}
+// subtract's argument
+subtract(5, 5);
